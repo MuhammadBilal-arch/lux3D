40caa813230c205a3ce1a068d6cc3d005d78aafe
Muhammad Bilal <ExpBilal@gmail.com>
2024-06-27 02:57:55 -0700

    added: button replaced owl-carousel with react-slick

--- components/carousel/offers.js
+++ components/carousel/offers.js
@@ -1,15 +1,13 @@
 'use client'
-import React, { useEffect, useRef, useState } from 'react'
-import dynamic from 'next/dynamic';
-import 'owl.carousel/dist/assets/owl.carousel.css'
-import 'owl.carousel/dist/assets/owl.theme.default.css'
-import { ASSETS } from '@/public/path'
+import React, { useRef, useState } from 'react';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import { ASSETS } from '@/public/path';
 import { HiMiniArrowSmallRight, HiMiniArrowSmallLeft } from "react-icons/hi2";
 
-
-const OwlCarousel = dynamic(() => import('react-owl-carousel'), { ssr: false });
 export const WeOffers = () => {
-    const [currentIndex, setCurrentIndex] = useState(0);
+    const sliderRef = useRef(null);
 
     const LANGUAGES = [
         {
@@ -24,91 +22,95 @@ export const WeOffers = () => {
         {
             IMG: ASSETS.OUR_PROJECT.OUR_PROJECT_4,
         },
-    ]
+    ];
 
-    const responsive = {
-        0: {
-            items: 3,
-        },
-        600: {
-            items: 3,
-        },
-        1000: {
-            items: 4,
-        },
-        1300: {
-            items: 4.5,
-        },
-    }
+    const settings = {
+        dots: false,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 4.5,
+        slidesToScroll: 1,
+        responsive: [
+            {
+                breakpoint: 1300,
+                settings: {
+                    slidesToShow: 4.5,
+                },
+            },
+            {
+                breakpoint: 1000,
+                settings: {
+                    slidesToShow: 3,
+                },
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                },
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                },
+            },
+        ],
+    };
 
     const handlePrevClick = () => {
-        setCurrentIndex(prevIndex => (prevIndex > 0 ? prevIndex - 1 : LANGUAGES.length - 1));
+        if (sliderRef.current) {
+            sliderRef.current.slickPrev();
+        }
     };
 
     const handleNextClick = () => {
-
-        setCurrentIndex(prevIndex => (prevIndex < LANGUAGES.length - 1 ? prevIndex + 1 : 0));
+        if (sliderRef.current) {
+            sliderRef.current.slickNext();
+        }
     };
 
-    console.log(currentIndex,"currentIndex")
     return (
         <div
             id="services"
-            className="bg-gray-extralight page-padding text-center space-y-8 py-24 overflow-hidden  text-purple-primary z-30">
+            className="bg-gray-extralight page-padding text-center space-y-8 py-24 overflow-hidden text-purple-primary z-30"
+        >
             <div className="">
                 <div className="overflow-hidden space-y-8">
                     <div className='flex items-center justify-between pr-10'>
                         <div className="text-aqua font-semibold text-xl lg:text-2xl xl:text-4xl 2xl:text-5xl">Our Services</div>
                         <div className='flex space-x-2'>
-                            <div
-                                onClick={handlePrevClick}
-                                className='cursor-pointer h-12 w-12 rounded-full text-white flex items-center justify-center bg-aqua text-2xl'><HiMiniArrowSmallLeft /></div>
-                            <div
-                                onClick={handleNextClick}
-                                className='cursor-pointer h-12 w-12 rounded-full text-white flex items-center justify-center bg-aqua text-2xl'><HiMiniArrowSmallRight /></div>
+                            <div className='cursor-pointer h-12 w-12 rounded-full text-white flex items-center justify-center bg-aqua text-2xl'
+                                onClick={handlePrevClick}><HiMiniArrowSmallLeft /></div>
+                            <div className='cursor-pointer h-12 w-12 rounded-full text-white flex items-center justify-center bg-aqua text-2xl'
+                                onClick={handleNextClick}><HiMiniArrowSmallRight /></div>
                         </div>
                     </div>
-                    <OwlCarousel
-                        // ref={carouselRef}
-                        className="owl-theme"
-                        items={4}
-                        loop
-                        onChanged={(event) => setCurrentIndex(event.page.index)}
-                        margin={10}
-                        autoplay={false}
-                        autoplayTimeout={4000}
-                        autoplaySpeed={250}
-                        navSpeed={250}
-                        active={currentIndex}
-                        nav={false}
-                        dots={false}
-                        responsive={responsive}
-                    >
-                        {LANGUAGES.map((items, index) => {
-                            return (
-                                <div className="h-96 relative group rounded-2xl" key={index}>
-                                    <div className='absolute cursor-pointer inset-0 hidden group-hover:bg-black/40 p-5 h-full rounded-2xl transition duration-300 delay-150 z-10  group-hover:flex flex-col items-center justify-end'>
-                                        <div className='text-left'>
-                                            <div className='font-semibold text-white text-sm md:text-base lg:text-xl'>
-                                                Pool Safety Tips
-                                            </div>
-                                            <div className='text-xs text-white font-light lg:text-sm mt-1'>
-                                                Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document.
-                                            </div>
+                    <Slider {...settings} ref={sliderRef}>
+                        {LANGUAGES.map((items, index) => (
+                            <div className={`h-96 relative group rounded-2xl px-3`} key={index}>
+                                <div className='absolute cursor-pointer inset-0 mx-3 hidden group-hover:bg-black/40 p-5 h-full rounded-2xl transition duration-300 delay-150 z-10 group-hover:flex flex-col items-center justify-end'>
+                                    <div className='text-left'>
+                                        <div className='font-semibold text-white text-sm md:text-base lg:text-xl'>
+                                            Pool Safety Tips
+                                        </div>
+                                        <div className='text-xs text-white font-light lg:text-sm mt-1'>
+                                            Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document.
                                         </div>
                                     </div>
-                                    <img
-                                        src={items.IMG.src}
-                                        alt=""
-                                        className="rounded sm:rounded-2xl object-fill  min-h-96 sm:object-cover cursor-pointer"
-                                    />
                                 </div>
-                            )
-                        })}
-                    </OwlCarousel>
+                                <img
+                                    src={items.IMG.src}
+                                    alt=""
+                                    className="rounded sm:rounded-2xl object-fill min-h-96 sm:object-cover cursor-pointer"
+                                />
+                            </div>
+                        ))}
+                    </Slider>
                 </div>
             </div>
         </div>
-    )
-}
+    );
+};
+
 
